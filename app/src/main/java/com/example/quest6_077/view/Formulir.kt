4aaaa18e105bba9171cece97b5d30ac870f81aff
salsadillaazkhiyahputri <salsadillaazkhiyahputri@gmail.com>
2025-11-20 14:15:29 +0700

    menambahkan   state txtNama

--- app/src/main/java/com/example/quest6_077/view/Formulir.kt
+++ app/src/main/java/com/example/quest6_077/view/Formulir.kt
@@ -47,13 +47,7 @@ fun FormIsian(
 ){
 
     var txtNama by rememberSaveable { mutableStateOf("") }
-    var txtAlamat by remember { mutableStateOf("") }
-    var txtGender by remember { mutableStateOf("") }
-    val listData: MutableList<String> = mutableListOf(
-        txtNama,
-        txtGender,
-        txtAlamat
-    )
+     
     Scaffold (modifier= Modifier,
         {
             TopAppBar(
